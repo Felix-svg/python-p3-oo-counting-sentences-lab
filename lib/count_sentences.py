4bdbe42723c58f54dd426487b7291f4a6815f37c
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-05 21:00:23 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+
+  def is_sentence(self):
+    if not isinstance(self.value, str):
+      return "The value must be a string."
+    if self.value and self.value[-1] == '.':
+      return True
+    else:
+      return False
+    
+  def is_question(self):
+    if not isinstance(self.value, str):
+      return "The value must be a string."
+    if self.value and self.value[-1] == '?':
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if not isinstance(self.value, str):
+      return "The value must be a string."
+    if self.value and self.value[-1] == '!':
+      return True
+    else:
+      return False
+    
+  def count_sentences(self):
+    if not isinstance(self.value, str):
+      return "The value must be a string."
+    return len(self.value.split('.'))
+    
